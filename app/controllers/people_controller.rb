b21dc72ff1a0e3ab68ce938c1516643e1d885f24
Maksim Masalitin <MaksMasalitin@yandex.ua>
2012-11-12 19:50:38 +0200

    Sceleton of profile page, creating new person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,8 @@
 class PeopleController < ApplicationController
+
+  def cerate
+    @person = Person.new(params[:person])	
+    @person.save
+    redirect_to "person/profile"
+  end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -3,8 +3,12 @@ class Person
   include DataMapper::Resource
 
   property :id, Serial
-  property :name, String
-  property :surname, String
+  property :name, String, :required => true
+  property :surname, String, :required => true
   property :email, String, :required => true
+  property :password, String, :required => true
+  property :nickname, String
+
+
 
 end
