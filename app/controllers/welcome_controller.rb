85839c29c93d2aa556cc5b0e8b1bdac462e50381
Masalitin Maksim <MaksMasalitin@yandex.ua>
2014-03-03 14:50:34 +0200

    Clean up git for new heroku app

--- app/controllers/welcome_controller.rb
+++
@@ -1,8 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-  end
-
-  def registration
-    @person = Person.new  	
-  end
-end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Social::Application.config.secret_token = '94dfb68f521b26440b5da0a5eb2a1bd259a95134ba1244ba44e719314cebb39466e9ad430c51bb84e118040acce36f40d6e7375c327e93cbc59c1b4b996a849d'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MorningStar::Application.config.secret_key_base = '85b1352171c83a0b63a2bd42f851c33f995767f0cbd7b7b3202cc69c23fbcd6bac65fd8a919429263688a13958b793e12320eb2082b1f1631b0510bb820a94cd'
